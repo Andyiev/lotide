e310448f48f64814abea437137cdb0063425277c
Andyiev <ayevdokymenko@gmail.com>
2021-05-25 02:18:22 +0000

    tail.js fixing

--- tail.js
+++ tail.js
@@ -1,22 +1,22 @@
-function tail(array) {
+let tail = function(array) {
   array.shift();
   console.log(array);
   return array;
-}
+};
 const assertEqual = function(actual, expected) {
   if (actual.length !== expected.length) {
-   return "False";
+    return "False";
   } else {
   // comapring each element of array
     for (let i = 0; i < actual.length; i++) {
       if (actual[i] === expected[i]) {
-      console.log("Assertion Passed: " + [actual] + " === " + [expected]);
+        console.log("Assertion Passed: " + [actual] + " === " + [expected]);
       } else {
-      console.log("Assertion Failed: " + [actual] + " !== " + [expected]);
+        console.log("Assertion Failed: " + [actual] + " !== " + [expected]);
       }
     }
   }
-}
+};
 const result = tail(["Hello", "Lighthouse", "Labs"]);
-assertEqual(result, ["Lighthouse", "Labs"]); 
+assertEqual(result, ["Lighthouse", "Labs"]);
 // => will always fail!
