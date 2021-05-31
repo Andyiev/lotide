101f0da3acc413a12afc547bf94bc4fbe8ba0a67
Andyiev <ayevdokymenko@gmail.com>
2021-05-31 21:53:23 +0000

    some editions

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -19,25 +19,6 @@ const assertArraysEqual = function(actual, expected) {
   }
 };
 
-//assertArraysEqual([1,2,3],[2,3,4]);
-
-/*const eqArrays = function(arrOne,arrTwo) {
-  let note = [];
-  if (arrOne.length !== arrTwo.length) {
-    note = "The arrays are not equal";
-    return note;
-  } else {
-    for (let i = 0; i < arrOne.length; i++) {
-      if (arrOne[i] !== arrTwo[i]) {
-        note = "The arrays are not equal";
-        return note;
-      }
-    }
-    note = "The arrays are equal";
-    return note;
-  }
-};
-*/
 //console.log("[1,2,3] and [1,2,3]. " + eqArrays([1,2,3],[1,2,3]));
 //console.log("[1,2,3] and [1,2,3]. " + assertArraysEqual([1,///2,3],[1,2,3]));
 
