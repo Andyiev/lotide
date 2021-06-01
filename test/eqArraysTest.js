ac58b0dc081e992904742984095f6b75c356a9ea
Andyiev <ayevdokymenko@gmail.com>
2021-06-01 01:48:16 +0000

    renew the file

--- test/eqArraysTest.js
+++ test/eqArraysTest.js
@@ -1,4 +1,11 @@
-const assertEqual = require("../assertEqual");
-const eqArrays = require("../eqArrays");
-eqArrays(["1", "2", "3"], ["1", "2", "3"]) // => true
-eqArrays(["1", "2", "3"], ["1", "2", 3]) // => false
+const assert = require('chai').assert;
+const eqArrays = require('../eqArrays');
+
+describe("#eqArrays", () => {
+  it("returns true for [1, 2, 3] and [1, 2, 3]", () => {
+    assert.equal(eqArrays([1, 2, 3], [1, 2, 3]), true);
+  });
+  it("returns false for [1, 2, 3] and [3, 2, 1]", () => {
+    assert.equal(eqArrays([1, 2, 3], [3, 2, 1]), false);
+  });
+});
