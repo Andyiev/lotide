504a627e4a0324050aca245ce587a1dfa94cfda4
Andyiev <ayevdokymenko@gmail.com>
2021-06-01 03:05:47 +0000

    modifying assertArrayEqual, assertEqual test/assertEqualTest and index.js

--- assertEqual.js
+++ assertEqual.js
@@ -4,8 +4,10 @@ const assertEqual = function(actual, expected) {
   if (actual === expected) {
     // console.log("Assertion Passed: " + [actual] + " === " + [expected]);
     console.log(`${pass} ${actual} === ${expected}`);
+    return true;
   } else {
     console.log("Assertion Failed: " + actual + " !== " + expected);
+    return false;
   }
 };
 // TEST CODE
--- test/assertEqualTest.js
+++ test/assertEqualTest.js
@@ -1,4 +1,19 @@
+const assert = require('chai').assert;
+const assertEqual = require('../assertEqual');
+
+describe("#assertEqual", () => {
+  it("returns true for \"Andrew\" and \"Andrew\"", () => {
+    assert.deepEqual(assertEqual(("Andrew"),("Andrew")), true);
+  });
+  it("returns true for 1234 and 1234", () => {
+    assert.deepEqual(assertEqual((1234),(1235)), false);
+  });
+});
+
+// This is my old code without mocha chai
+/*
 const assertEqual = require('../assertEqual');
 assertEqual(1, 1);
 assertEqual(2,30);
 assertEqual("Andrew","Andrew");
+*/
